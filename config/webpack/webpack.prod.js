aa5c6011fce34666e15d679f7198a0690d6292cb
Mircea Staicu <md.staicu@yahoo.com>
2020-09-08 17:23:37 +0100

    update webpack copy plugin arguments

--- config/webpack/webpack.prod.js
+++ config/webpack/webpack.prod.js
@@ -6,7 +6,9 @@ module.exports = {
   mode: 'production',
   devtool: 'source-map',
   plugins: [
-    new CopyWebpackPlugin([{ from: 'public' }]),
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'public' }],
+    }),
     new HtmlWebpackPlugin({ template: './public/index.html' }),
     new Dotenv({
       path: './.env.production',
